fe3f03b5ae4ef4d57e9335f6e875c077c68aad75
Rayehan Kausar <rayehan@gmail.com>
2023-08-01 00:23:44 -0400

    updated main by setting set and get values

--- Assignment_3/src/Main.java
+++ Assignment_3/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! ");
+
+        UserInput name = new UserInput();
+        UserInput email = new UserInput();
+        UserInput password = new UserInput();
+        name.setuserName("John");
+        email.seteMail("test@test.com");
+        password.setPassWord("abcd123");
+        System.out.println(name.getuserName());
+        System.out.println(email.geteMail());
+        System.out.println(password.getPassWord());
     }
 }
